7e22aa38cbbdfe54baab7e13179092f2883311eb
Evelina <evelina.enqvist@brightstraining.com>
2024-12-05 11:33:34 +0100

    Update submodule changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About  from "./pages/about"
 import Profile from "./pages/profile"
 import Budget from './pages/budget';
+import { Menu } from './menu';
 
  
 export default function App() {
   return (
     <>
       <h1>APP</h1>
-      <Link to="/">Home</Link>&nbsp;
+     {/*} <Link to="/">Home</Link>&nbsp;
       <Link to="/about">About</Link>&nbsp;
       <Link to="/profile">Profile</Link>&nbsp;
-      <Link to="/budget">Budget</Link>&nbsp;
+      <Link to="/budget">Budget</Link>&nbsp;*/}
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/budget" element={<Budget />} />
-        
+      <Route path="/" element ={<Menu />}>
+      <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/budget" element={<Budget />} />
+        </Route>
       </Routes>
 
     
